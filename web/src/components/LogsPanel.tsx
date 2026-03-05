18ad9e6694d66b26a73e1aef603bbfa309f71b51
Tyler Vincent <tylerlv3@gmail.com>
2026-03-04 20:25:32 -0800

    Grid rezising to fit when side panel is open. Rename app to remove prototype:

--- web/src/components/LogsPanel.tsx
+++ web/src/components/LogsPanel.tsx
@@ -347,7 +347,9 @@ export default function LogsPanel({
                         onClick={onClose}
                         aria-label="Close logs"
                     >
-                        X
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" style={{ width: "20px", height: "20px" }}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
                     </button>
                 </div>
 
