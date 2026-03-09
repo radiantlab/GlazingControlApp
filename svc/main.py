548b708e6a02c3e17fcfffd835a931aad52e714a
Tyler Vincent <tylerlv3@gmail.com>
2026-03-08 20:04:47 -0700

    feat: Add docker configuration

--- svc/main.py
+++ svc/main.py
@@ -113,7 +113,7 @@ def create_app() -> FastAPI:
     # CORS for local web dev
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+        allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost", "http://127.0.0.1"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
