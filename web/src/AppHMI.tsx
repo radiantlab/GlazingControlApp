6878e7d40cb164b6eee53d00349ac6384101ab6d
imckee-dev <ianmckee5@gmail.com>
2026-04-26 16:19:28 -0700

    docs page largely stylistic. Todo, rest of text: Need to add group control and windows sections.

--- web/src/AppHMI.tsx
+++ web/src/AppHMI.tsx
@@ -584,10 +584,11 @@ export default function AppHMI() {
                             Routines
                         </button>
 
-                        <Link to="/docs">
+                        <Link to="/docs"
+                        >
                             <button
-                                className="hmi-docs-btn"
-                                title="View Docs"
+                            className="hmi-manage-btn"
+                            title="View Docs"
                             >
                                 Docs
                             </button>
